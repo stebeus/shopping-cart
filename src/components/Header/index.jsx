5f632a4aa25de8c5300c695e4a285eb58d6f51cc
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-05 18:01:23 -0300

    fix: export `BASE_URL` to fix GitHub pages root paath

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,16 +2,18 @@ import { Link } from 'react-router';
 
 import './index.css';
 
+import { BASE_URL } from '@/routes.jsx';
+
 export function Header() {
   return (
     <header>
       <h1 className="logo">
-        <Link to="/">Shopping cart</Link>
+        <Link to={BASE_URL}>Shopping cart</Link>
       </h1>
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to={BASE_URL}>Home</Link>
           </li>
           <li>
             <Link to="shop">Shop</Link>
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { App } from './App.jsx';
 import { Home } from './pages/Home.jsx';
 
-const BASE_URL = import.meta.env.BASE_URL;
+export const BASE_URL = import.meta.env.BASE_URL;
 
 export const routes = [
   {
