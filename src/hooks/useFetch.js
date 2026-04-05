39ed3af74ce1a5d68bd851480384434c6262cbf9
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-05 16:44:48 -0300

    feat: add fetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+async function fetchData(url, ...[dataSetter, errorSetter, isLoadingSetter]) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    dataSetter(data);
+  } catch (error) {
+    errorSetter(error.message);
+  } finally {
+    isLoadingSetter(false);
+  }
+}
+
+export function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchData(url, setData, setError, setIsLoading);
+  }, [url]);
+
+  return { data, isLoading, error };
+}
