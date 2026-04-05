467306f3966cbe226509173ded181746f6206aad
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-05 14:07:34 -0300

    feat: add navigation links

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,6 +8,19 @@ export function Header() {
       <h1 className="logo">
         <Link to="/">Shopping cart</Link>
       </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
