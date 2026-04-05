b294a7ec9a4143851e24b9055ba393b93ffdfa8b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-05 17:44:07 -0300

    feat: route Home page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,10 @@
 import { App } from './App.jsx';
+import { Home } from './pages/Home.jsx';
 
 export const routes = [
   {
     path: '/*',
     element: <App />,
+    children: [{ index: true, element: <Home /> }],
   },
 ];
