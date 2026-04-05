db8b05774fd8cfbd35da42f5aeb5b54a29229cc8
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-05 17:45:00 -0300

    fix: read base url constant The Home page does not render by default, as React Router is not resolving the root path to the base URL.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,11 @@
 import { App } from './App.jsx';
 import { Home } from './pages/Home.jsx';
 
+const BASE_URL = import.meta.env.BASE_URL;
+
 export const routes = [
   {
-    path: '/*',
+    path: BASE_URL,
     element: <App />,
     children: [{ index: true, element: <Home /> }],
   },
