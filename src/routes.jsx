f47afafbc9ba1b1c0001d8c99bc97bdb058afdac
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-05 18:03:27 -0300

    feat: route new pages

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,7 @@
 import { App } from './App.jsx';
+import { Cart } from './pages/Cart.jsx';
 import { Home } from './pages/Home.jsx';
+import { Shop } from './pages/Shop.jsx';
 
 export const BASE_URL = import.meta.env.BASE_URL;
 
@@ -7,6 +9,10 @@ export const routes = [
   {
     path: BASE_URL,
     element: <App />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'shop', element: <Shop /> },
+      { path: 'cart', element: <Cart /> },
+    ],
   },
 ];
