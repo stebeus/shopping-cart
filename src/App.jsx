e269feda82e46db015b17dd9e6dd7a3ecb898125
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-05 17:44:23 -0300

    feat: render components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Outlet } from 'react-router';
+
 import './assets/App.css';
 
+import { Footer } from './components/Footer/index.jsx';
+import { Header } from './components/Header/index.jsx';
+
 export function App() {
-  return;
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 }
