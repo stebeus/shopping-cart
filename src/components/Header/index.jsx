e237a41fa83498316ddcd2e570317211c035ef93
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-05 18:19:56 -0300

    fix!: remove base URL
    Host page in other providers, since GitHub pages require the usage of a
    base URL constant, on which adds an unnecessary step for adding internal
    links.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,18 +2,16 @@ import { Link } from 'react-router';
 
 import './index.css';
 
-import { BASE_URL } from '@/routes.jsx';
-
 export function Header() {
   return (
     <header>
       <h1 className="logo">
-        <Link to={BASE_URL}>Shopping cart</Link>
+        <Link to="/">Shopping cart</Link>
       </h1>
       <nav>
         <ul>
           <li>
-            <Link to={BASE_URL}>Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="shop">Shop</Link>
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,11 +3,9 @@ import { Cart } from './pages/Cart.jsx';
 import { Home } from './pages/Home.jsx';
 import { Shop } from './pages/Shop.jsx';
 
-export const BASE_URL = import.meta.env.BASE_URL;
-
 export const routes = [
   {
-    path: BASE_URL,
+    path: '/',
     element: <App />,
     children: [
       { index: true, element: <Home /> },
