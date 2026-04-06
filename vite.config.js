490923e6f586996213edcf5e65d822792bc5c55b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-06 13:02:38 -0300

    chore(vite): sort imports

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import path from 'node:path';
+import { cloudflare } from '@cloudflare/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [react(), cloudflare()],
