166d975f9306e775d2dcb6ebec12a8f10fad3d2a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-13 15:05:02 -0300

    feat: add Error page

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import { Link, useRouteError } from 'react-router';
+
+export function ErrorPage() {
+  const error = useRouteError();
+
+  console.error(error);
+
+  return (
+    <>
+      <h2>{error.status}</h2>
+      <p>{error.statusText ?? error.message}</p>
+      <Link to="/">Return to home</Link>
+    </>
+  );
+}
