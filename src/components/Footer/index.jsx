4664febfbb398627360b722ce9fbd4e928d7f811
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-13 14:00:55 -0300

    feat: add Header component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,20 @@
+import { ExternalLink } from '@/components/ui/ExternalLink.jsx';
+import { SvgIcon } from '@/components/ui/SvgIcon.jsx';
+
+import './index.css';
+
+export function Footer() {
+  return (
+    <footer>
+      <p className="copyright">
+        © Stebeus 2025.{' '}
+        <ExternalLink to="https://opensource.org/license/mit">
+          MIT License
+        </ExternalLink>
+      </p>
+      <ExternalLink to="https://github.com/stebeus">
+        <SvgIcon id="github" alt="GitHub" />
+      </ExternalLink>
+    </footer>
+  );
+}
