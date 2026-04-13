7c0e3de177167ccfc6b34c7e55825f57bedf4030
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-13 16:44:57 -0300

    feat: render fetched products grid

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,7 +1,22 @@
+import { ErrorMessage } from '@/components/ui/ErrorMessage.jsx';
+import { useFetch } from '@/hooks/useFetch.js';
+import { Card } from './Card.jsx';
+
 export function Products() {
+  const api = 'https://fakestoreapi.com/products';
+  const [products, isLoading, error] = useFetch(api);
+
+  const createCard = ({ id, title, price, image }) => (
+    <Card key={id} title={title} price={price} imgUrl={image} />
+  );
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <ErrorMessage description={error} />;
+
   return (
     <>
       <h2>Products</h2>
+      <div className="grid">{products.map(createCard)}</div>
     </>
   );
 }
