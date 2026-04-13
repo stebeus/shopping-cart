866641ff74f3b73005a22e9be7b3098432eda3e7
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-13 17:04:10 -0300

    feat: render items

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,7 +1,20 @@
-export function Cart() {
+import { Item } from './Item.jsx';
+
+export function Cart({ items }) {
+  const createItem = ({ id, title, price, quantity, image }) => (
+    <Item
+      key={id}
+      title={title}
+      price={price}
+      quantity={quantity}
+      imgUrl={image}
+    />
+  );
+
   return (
     <>
       <h2>Cart</h2>
+      <ul className="products">{items.map(createItem)}</ul>
     </>
   );
 }
