81addfe49e6cbd766e8fe1e16ffb6a353cdd8e24
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-13 15:16:11 -0300

    feat: route pages

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,19 @@
 import { App } from './App.jsx';
 
+import { Cart } from './pages/Cart/index.jsx';
+import { ErrorPage } from './pages/ErrorPage.jsx';
+import { Home } from './pages/Home.jsx';
+import { Products } from './pages/Products/index.jsx';
+
 export const routes = [
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'products', element: <Products /> },
+      { path: 'cart', element: <Cart /> },
+    ],
   },
 ];
