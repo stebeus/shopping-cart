dc8f28f7e602c947cc6704723dcc6ef567244a8a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-14 14:04:54 -0300

    feat: readd ProductInfo component

---
+++ src/components/ProductInfo.jsx
@@ -0,0 +1,17 @@
+import { Field } from './ui/Field.jsx';
+
+export function ProductInfo({ title, price, quantity, image, ...fieldProps }) {
+  return (
+    <>
+      <img src={image} alt="" width={100} height={100} />
+      <h3>{title}</h3>
+      <p className="price">$ {price}</p>
+      <Field
+        label="Quantity: "
+        type="number"
+        value={quantity}
+        {...fieldProps}
+      />
+    </>
+  );
+}
