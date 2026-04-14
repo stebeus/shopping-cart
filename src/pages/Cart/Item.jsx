a8c0622646419c7d8329083cd968bb16aeb31690
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-14 14:36:10 -0300

    feat: add handleQuantity

--- src/pages/Cart/Item.jsx
+++ src/pages/Cart/Item.jsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import { useOutletContext } from 'react-router';
 
 import { ProductInfo } from '@/components/ProductInfo.jsx';
 import { Button } from '@/components/ui/Button.jsx';
 
 export function Item({ id, title, price, quantity, image }) {
+  const [newQuantity, setQuantity] = useState(quantity);
   const [purchases, setPurchases] = useOutletContext();
+
+  function handleQuantity({ target }) {
+    const newQuantity = target.value;
+    const item = target.closest('.item');
+
+    if (newQuantity < 1) {
+      const isNotPurchase = ({ id }) => id !== item.id;
+      setPurchases(purchases.filter(isNotPurchase));
+      return;
+    }
+
+    const isPurchase = ({ id }) => id === item.id;
+
+    const purchase = purchases.find(isPurchase);
+    const newPurchase = { ...purchase, quantity: Number(newQuantity) };
+
+    const setNewPurchases = (purchase) =>
+      purchase.id === item.id ? newPurchase : purchase;
+
+    setQuantity(newQuantity);
+    setPurchases(purchases.map(setNewPurchases));
+  }
 
   function handleRemove({ target }) {
     const item = target.closest('.item');
@@ -17,8 +41,9 @@ export function Item({ id, title, price, quantity, image }) {
       <ProductInfo
         title={title}
         price={price}
-        quantity={quantity}
+        quantity={newQuantity}
         image={image}
+        onChange={handleQuantity}
       />
       <Button onClick={handleRemove}>Remove</Button>
     </li>
