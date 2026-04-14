ae6a7e48e504463c7aa0d6a3e1ea7d5171263f0a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-14 14:16:46 -0300

    feat: add remove button

--- src/pages/Cart/Item.jsx
+++ src/pages/Cart/Item.jsx
@@ -1,6 +1,17 @@
+import { useOutletContext } from 'react-router';
+
 import { ProductInfo } from '@/components/ProductInfo.jsx';
+import { Button } from '@/components/ui/Button.jsx';
 
 export function Item({ id, title, price, quantity, image }) {
+  const [purchases, setPurchases] = useOutletContext();
+
+  function handleRemove({ target }) {
+    const item = target.closest('.item');
+    const isNotPurchase = ({ id }) => id !== item.id;
+    setPurchases(purchases.filter(isNotPurchase));
+  }
+
   return (
     <li id={id} className="item">
       <ProductInfo
@@ -8,8 +19,8 @@ export function Item({ id, title, price, quantity, image }) {
         price={price}
         quantity={quantity}
         image={image}
-        onChange={handleQuantity}
       />
+      <Button onClick={handleRemove}>Remove</Button>
     </li>
   );
 }
