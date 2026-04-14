c00dae897f0126f05279b40276ef705d6de2c404
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-14 09:22:02 -0300

    refactor! improve Products page

--- src/pages/Products/Card.jsx
+++ src/pages/Products/Card.jsx
@@ -1,16 +1,11 @@
+import { ProductInfo } from '@/components/ProductInfo.jsx';
 import { Button } from '@/components/ui/Button.jsx';
-import { Field } from '@/components/ui/Field.jsx';
 
 export function Card({ id, title, price, image }) {
-  function handlePurchase() {}
-
   return (
     <article id={id} className="card">
-      <img src={image} alt="" width={100} height={100} />
-      <h3>{title}</h3>
-      <p className="price">$ {price}</p>
-      <Field label="Quantity" type="number" defaultValue={1} min={1} />
-      <Button onClick={handlePurchase}>Add to cart</Button>
+      <ProductInfo title={title} price={price} image={image} />
+      <Button>Add to cart</Button>
     </article>
   );
 }
