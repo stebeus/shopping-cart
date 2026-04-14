1a6de35c529d18720fd119ce373e4efd6e0a0160
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-14 09:56:22 -0300

    refactor: use state for the product quantity

--- src/pages/Products/Card.jsx
+++ src/pages/Products/Card.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 import { ProductInfo } from '@/components/ProductInfo.jsx';
 import { Button } from '@/components/ui/Button.jsx';
 
 export function Card({ id, title, price, image }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantity = ({ target: { value } }) => setQuantity(value);
+
   return (
     <article id={id} className="card">
-      <ProductInfo title={title} price={price} image={image} />
+      <ProductInfo
+        title={title}
+        price={price}
+        image={image}
+        quantity={quantity}
+        min={1}
+        onChange={handleQuantity}
+      />
       <Button>Add to cart</Button>
     </article>
   );
