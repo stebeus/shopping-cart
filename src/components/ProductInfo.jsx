e134b4a8d6a9a97eae938db3deb39bc1b683834f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-14 09:55:59 -0300

    refactor: simplify ProductInfo component

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,23 +1,12 @@
 import { Field } from './ui/Field.jsx';
 
-export function ProductInfo({
-  title,
-  price,
-  image,
-  defaultQuantity = 1,
-  min = 1,
-}) {
+export function ProductInfo({ title, price, image, quantity, ...fieldProps }) {
   return (
     <>
       <img src={image} alt="" width={100} height={100} />
       <h3>{title}</h3>
       <p className="price">$ {price}</p>
-      <Field
-        label="Quantity"
-        type="number"
-        defaultValue={defaultQuantity}
-        min={min}
-      />
+      <Field label="Quantity" type="number" value={quantity} {...fieldProps} />
     </>
   );
 }
