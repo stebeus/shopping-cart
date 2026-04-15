3ecd1d8f2261b5c578cf444333c440209b60e0d5
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-15 11:15:17 -0300

    refactor!: rename SvgIcon to Icon

---
+++ src/components/ui/Icon.jsx
@@ -0,0 +1,8 @@
+export function Icon({ id, title }) {
+  return (
+    <svg className="icon" width={24} height={24} fill="currentColor">
+      <title>{title}</title>
+      <use href={`icons.svg#${id}-icon`}></use>
+    </svg>
+  );
+}
--- src/components/ui/SvgIcon.jsx
+++
@@ -1,8 +0,0 @@
-export function SvgIcon({ id, title }) {
-  return (
-    <svg className="svgIcon" width={24} height={24}>
-      <title>{title}</title>
-      <use href={`icons.svg#${id}`}></use>
-    </svg>
-  );
-}
