262ea7edc56fabf361ab84788a0e3afcd8ef1b69
stebeus <189894010+stebeus@users.noreply.github.com>
2026-04-16 07:11:33 -0300

    refactor! improve carts page code

--- src/pages/cart/Item.jsx
+++ src/pages/cart/Item.jsx
@@ -1,44 +1,47 @@
 import { useState } from 'react';
 import { useOutletContext } from 'react-router';
 
-import { ProductInfo } from '@/components/ProductInfo.jsx';
+import { ProductData } from '@/components/ProductData.jsx';
 import { Button } from '@/components/ui/Button.jsx';
 
 export function Item({ id, title, price, quantity, image }) {
-  const [newQuantity, setQuantity] = useState(quantity);
+  const [newQuantity, setNewQuantity] = useState(quantity);
   const [purchases, setPurchases] = useOutletContext();
 
-  function handleQuantity({ target }) {
-    const newQuantity = target.value;
-    const item = target.closest('.item');
+  function removePurchase(itemId) {
+    const isNotPurchase = ({ id }) => id !== itemId;
+    setPurchases(purchases.filter(isNotPurchase));
+  }
 
-    if (newQuantity < 1) {
-      const isNotPurchase = ({ id }) => id !== item.id;
-      setPurchases(purchases.filter(isNotPurchase));
+  function handleQuantity({ target }) {
+    const quantity = target.value;
+    const itemId = target.closest('.item').id;
+
+    if (quantity < 1) {
+      removePurchase(itemId, purchases);
       return;
     }
 
-    const isPurchase = ({ id }) => id === item.id;
+    const isPurchase = ({ id }) => id === itemId;
 
     const purchase = purchases.find(isPurchase);
-    const newPurchase = { ...purchase, quantity: Number(newQuantity) };
+    const newPurchase = { ...purchase, quantity: Number(quantity) };
 
     const setNewPurchases = (purchase) =>
-      purchase.id === item.id ? newPurchase : purchase;
+      purchase.id === itemId ? newPurchase : purchase;
 
-    setQuantity(newQuantity);
+    setNewQuantity(quantity);
     setPurchases(purchases.map(setNewPurchases));
   }
 
   function handleRemove({ target }) {
-    const item = target.closest('.item');
-    const isNotPurchase = ({ id }) => id !== item.id;
-    setPurchases(purchases.filter(isNotPurchase));
+    const itemId = target.closest('.item').id;
+    removePurchase(itemId, purchases);
   }
 
   return (
     <li id={id} className="item">
-      <ProductInfo
+      <ProductData
         title={title}
         price={price}
         quantity={newQuantity}
